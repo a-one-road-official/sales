243f7a1401f25972cdf049de5c941d4f9788029e
a-one-road-official <admin@a1-road.com>
2026-09-09 11:52:06 +0900

    Fix daily SLO deadline rollover to same JST day

--- production_controller.py
+++ production_controller.py
@@ -185,7 +185,7 @@ class QualifiedLeadProductionController:
         previous_report = self._report(previous)
         now = datetime.now(UTC)
         jst_now = now.astimezone(JST)
-        next_deadline = (jst_now + timedelta(days=1)).replace(
+        next_deadline = jst_now.replace(
             hour=23, minute=59, second=59, microsecond=0
         ).astimezone(UTC)
         self._set_config({
