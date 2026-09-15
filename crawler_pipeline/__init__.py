4ad1133734a5981d4ed1fd390d41d9600c44e88d
a-one-road-official <admin@a1-road.com>
2026-09-15 09:13:47 +0900

    Expose bounded intake runner

--- crawler_pipeline/__init__.py
+++ crawler_pipeline/__init__.py
@@ -1,5 +1,5 @@
 """Vertex-free deterministic company crawling pipeline."""
 
-__all__ = ["crawl_company", "classify_evidence"]
+__all__ = ["crawl_company", "classify_evidence", "run_bounded_batch"]
 
-from .pipeline import classify_evidence, crawl_company
+from .pipeline import classify_evidence, crawl_company\nfrom .runner import run_bounded_batch
