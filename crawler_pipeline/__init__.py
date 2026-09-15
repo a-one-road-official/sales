3b405d23d6480d0852506560360bcb866a58fe69
a-one-road-official <admin@a1-road.com>
2026-09-15 09:15:34 +0900

    Fix generated newline escaping

--- crawler_pipeline/__init__.py
+++ crawler_pipeline/__init__.py
@@ -2,4 +2,5 @@
 
 __all__ = ["crawl_company", "classify_evidence", "run_bounded_batch"]
 
-from .pipeline import classify_evidence, crawl_company\nfrom .runner import run_bounded_batch
+from .pipeline import classify_evidence, crawl_company
+from .runner import run_bounded_batch
